8b9d3a447a2deae1367f6677ece0070a21c33587
Adam Hunter <90648351+adam-hunter13@users.noreply.github.com>
2022-01-05 14:26:12 -0700

    deployment changes

--- src/App.js
+++ src/App.js
@@ -10,26 +10,23 @@ function App() {
   const [eventData, setEventData] = useState(events)
   const [loading, setLoading] = useState(false)
 
-  useEffect(() => {
-    // IF NASA GETS BACK WITH INFO UNBLOCK LINES 15-25 IN
-    // const fetchEvents = async () => {
-    //   setLoading(true)
-    //   const res = await fetch('https://eonet.gsfc.nasa.gov/api/v2.1/events?api_key=Tn92YFI4mOre4i9L9c43C6tC3hSeqZ1uxOM2iey8')
-    //   console.log(res)
-    //   const { events } = await res.json()
+  //   IF NASA GETS BACK WITH INFO UNBLOCK LINES 15-25 IN
+  // useEffect(() => {
+  //   const fetchEvents = async () => {
+  //     setLoading(true)
+  //     const res = await fetch('https://eonet.gsfc.nasa.gov/api/v2.1/events?api_key=Tn92YFI4mOre4i9L9c43C6tC3hSeqZ1uxOM2iey8')
+  //     console.log(res)
+  //     const { events } = await res.json()
 
-    //   setEventData(events)
-    //   setLoading(false)
-    // }
+  //     setEventData(events)
+  //     setLoading(false)
+  //   }
 
-    // fetchEvents ()
-
-    // IF NASA GETS BACK CODE THE FOLLOWING LINE OUT
-    setEventData(events)
-    console.log(eventData)
-
-  }, [])
-
+  //   fetchEvents ()
+    
+  // }, [])
+ 
+  
   return (
     <div>
       <Header />
