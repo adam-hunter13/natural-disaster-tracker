92a530fab8402302ba0a0d61ecfbb1a77b286e4b
Adam Hunter <90648351+adam-hunter13@users.noreply.github.com>
2022-01-05 14:19:57 -0700

    hard coded data

--- src/App.js
+++ src/App.js
@@ -3,23 +3,31 @@ import Map from "./components/Map";
 import Loader from "./components/Loader"
 import Header from './components/Header'
 import Legend from './components/Legend';
+import events from './components/events'
 
 
 function App() {
-  const [eventData, setEventData] = useState([])
+  const [eventData, setEventData] = useState(events)
   const [loading, setLoading] = useState(false)
 
   useEffect(() => {
-    const fetchEvents = async () => {
-      setLoading(true)
-      const res = await fetch('https://eonet.gsfc.nasa.gov/api/v2.1/events')
-      const { events } = await res.json()
+    // IF NASA GETS BACK WITH INFO UNBLOCK LINES 15-25 IN
+    // const fetchEvents = async () => {
+    //   setLoading(true)
+    //   const res = await fetch('https://eonet.gsfc.nasa.gov/api/v2.1/events?api_key=Tn92YFI4mOre4i9L9c43C6tC3hSeqZ1uxOM2iey8')
+    //   console.log(res)
+    //   const { events } = await res.json()
 
-      setEventData(events)
-      setLoading(false)
-    }
+    //   setEventData(events)
+    //   setLoading(false)
+    // }
 
-    fetchEvents ()
+    // fetchEvents ()
+
+    // IF NASA GETS BACK CODE THE FOLLOWING LINE OUT
+    setEventData(events)
+    console.log(eventData)
+
   }, [])
 
   return (
